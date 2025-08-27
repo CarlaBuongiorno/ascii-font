e1e8c872f7a9394433ceae77b25bba0089366c8b
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-08-27 09:51:31 +0200

    Add project requirements and requirements.txt

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Write a program to print ASCII Font versions of any text you give it. i.e.:
+
+> python ascii-font.py A word
+  *       * * *  ***  ***   ***
+ * *      * * * *   * *  *  *  *
+*   *      * *  *   * *  *  *  *
+*****      * *  *   * ****  *  *
+*   *      * *   ***  *   * *** 
+
+Be as creative as you like! Don't use any external libraries or APIs.'''
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
